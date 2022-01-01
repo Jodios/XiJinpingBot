e902d87f47283c38940f93fa185f52cff4dae3ab
Joel <joelandapophis@gmail.com>
2022-01-01 01:00:32 -0600

    init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const dotenv = require("dotenv").config()
 const figlet = require("figlet")
 const discordjs = require("discord.js")
+const firewall = require("./resources/firewall.json")
 
 const client = new discordjs.Client({
-	intents: ["GUILD_MESSAGES"]
+	intents: ["GUILD_MESSAGES", "GUILDS"]
+})
+
+client.on("messageCreate", (message) => {
+	firewall.forEach((badWord, i) => {
+		let matches = message.content.toLowerCase().match(`.*${badWord.toLowerCase()}.*`)
+		if(matches != undefined && matches.length != 0 ){
+			message.channel.send(":angry:")
+			return;
+		}
+	})
+
 })
 
 client.on("ready", () => {
